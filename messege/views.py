f4e99448baa187f5a0c3e10ab3a338d98026bfe8
Tarif Bin Mehedi <tarifbinmehedicrp@gmail.com>
2026-06-28 00:41:01 +0600

    createMessage

--- messege/views.py
+++ messege/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Message
 
 # Create your views here.
 def home(request):
 
-    messages = Message.objects.all().order_by('-created_at')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        text = request.POST.get('text')
 
-    return render(request, 'messge/home.html', {'messages': messages})
+        if name and text:
+            Message.objects.create(name=name, text=text)
+
+        return redirect('home')
+
+    messages = Message.objects.all().order_by('-created_at')
+    return render(request, 'message/home.html', {'messages': messages})
+
